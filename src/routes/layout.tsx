ad1f879bd6d23bee924bee7edbad00556e15d8f6
maiieul <45822175+maiieul@users.noreply.github.com>
2026-01-16 09:25:50 +0100

    chore: small commit

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,7 +2,7 @@ import { component$, Slot } from "@qwik.dev/core";
 import { RequestHandler } from "@qwik.dev/router/middleware/request-handler";
 
 export const onRequest: RequestHandler = ({ cacheControl }) => {
-  return cacheControl({ public: true, maxAge: 600 });
+  return cacheControl({ public: true, maxAge: 1200 });
 };
 
 export default component$(() => {
